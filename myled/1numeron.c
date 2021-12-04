2ffa7500020325e6086e31059b178c582f33467f
Bato Riko <s20c1101jd@s.chib>
2021-12-04 09:06:23 +0900

    kadai2

---
+++ myled/1numeron.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int Eranp[3], Branp[3];
+int n = 0, y = 0;
+void shuffle(char array[], int size)
+{
+    srand((int)time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        int j = rand() % size;
+        int t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+}
+void game(char user[], char rdom[])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (user[i] == rdom[i])
+            n++;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int w = 0; w < 3; w++)
+        {
+            if (rdom[i] == user[w])
+                y++;
+        }
+    }
+}
+int main()
+{
+    system("sudo insmod myled1.ko");
+    system("sudo chmod 666 /dev/myled0");
+    char array[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char user[3];
+    shuffle(array, 9);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%c", array[i]);
+    }
+    scanf("%c%c%c", &user[0], &user[1], &user[2]);
+    game(array, user);
+    printf("%d,%d\n", n, y);
+    if (n == 0)
+        system("echo E$n> /dev/myled0");
+    else if (y == 0)
+        system("echo B$y> /dev/myled0");
+    else
+        system("echo Q0> /dev/myled0");
+}
