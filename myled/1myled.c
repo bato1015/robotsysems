bdb309014b1c36e17598a29a094c9fc50e1aec66
Bato Riko <s20c1101jd@s.chib>
2021-12-05 18:09:45 +0900

    test

--- myled/1myled.c
+++ myled/1myled.c
@@ -63,10 +63,19 @@ static ssize_t led_write(struct file *filp, const char *buf, size_t count, loff_
     else if (c == 'c')
     {
         count_num = 3;
-        led_T(gpio[1], 3);
+        //led_T(gpio[1], 3);
     }
 
-    hantei(mode, count_num);
+    //hantei(mode, count_num);
+    if (mode == 1)
+        led_T(gpio[0], count_num);
+    else if (mode == 2)
+        led_T(gpio[1], count_num);
+    else if (mode == 3)
+    {
+        int n = 2;
+        led_T(gpio[2], n);
+    }
     msleep(100);
     for (i = 0; i < 3; i++)
         gpio_base[10] = 1 << gpio[i];
