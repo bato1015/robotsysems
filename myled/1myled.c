31ce7da3363c112b8c07d17023dbf8291974867c
Bato Riko <s20c1101jd@s.chib>
2021-12-05 14:08:18 +0900

    test

--- myled/1myled.c
+++ myled/1myled.c
@@ -15,9 +15,18 @@ static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
 static volatile u32 *gpio_base = NULL; //アドレスをマッピングするための配列をグローバルで定義している
-int gpio[] = {13, 19, 26, 20, 16, 12};
-int bzar = 25;
-
+int gpio[] = {13, 19, 25};
+void led_T(int gpio_num, int right_count)
+{
+    int i;
+    for (i = 0; i < right_count; i++)
+    {
+        gpio_base[7] = 1 << gpio_num;
+        msleep(25);
+        gpio_base[10] = 1 << gpio_num;
+        msleep(50);
+    }
+}
 static ssize_t led_write(struct file *filp, const char *buf, size_t count, loff_t *pos)
 {
     char c;
@@ -31,27 +40,14 @@ static ssize_t led_write(struct file *filp, const char *buf, size_t count, loff_
     printk(KERN_INFO "receive %c\n", c);
 
     if (c == 'E')
-    {
-        for (i = 0; i < n; i++)
-            gpio_base[7] = 1 << gpio[n];
-    }
+        led_T(gpio[0], n);
     else if (c == 'B')
-    {
-        for (i = 0; i < n; i++)
-            gpio_base[7] = 1 << gpio[n + 3];
-    }
+        led_T(gpio[1], n);
     else if (c == 'Q')
-    {
-        for (i = 0; i < 2; i++)
-        {
-            gpio_base[7] = 1 << bzar;
-            msleep(25);
-            gpio_base[10] = 1 << bzar;
-            msleep(50);
-        }
-    }
+        led_T(gpio[2], 2);
+
     msleep(1000);
-    for (i = 0; i < 6; i++)
+    for (i = 0; i < 3; i++)
         gpio_base[10] = 1 << gpio[i];
     return 1;
 }
@@ -75,6 +71,7 @@ static struct file_operations led_fops = {
 static int __init init_mod(void) //カーネルモジュールの初期化
 {
     int retval;
+    int i;
     retval = alloc_chrdev_region(&dev, 0, 1, "myled");
     if (retval < 0)
     {
@@ -97,22 +94,23 @@ static int __init init_mod(void) //カーネルモジュールの初期化
     }
     device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
     gpio_base = ioremap_nocache(0xfe200000, 0xA0);
-    const u32 led = 25;
-    const u32 index = led / 10;       //gpiosel2
-    const u32 shift = (led % 10) * 3; //15bit
-    const u32 mask = ~(0x7 << shift);
-    gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+    for (i = 0; i < 3; i++)
+    {
+        const u32 led = gpio[i];
+        const u32 index = led / 10;       //gpiosel2
+        const u32 shift = (led % 10) * 3; //15bit
+        const u32 mask = ~(0x7 << shift);
+        gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+        return 0;
+    }
+    static void __exit cleanup_mod(void) //
+    {
+        cdev_del(&cdv);
+        device_destroy(cls, dev);
+        class_destroy(cls);
+        unregister_chrdev_region(dev, 1);
+        printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
+    }
 
-    return 0;
-}
-static void __exit cleanup_mod(void) //
-{
-    cdev_del(&cdv);
-    device_destroy(cls, dev);
-    class_destroy(cls);
-    unregister_chrdev_region(dev, 1);
-    printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
-}
-
-module_init(init_mod);    //マクロで関数を登録
-module_exit(cleanup_mod); //同上
+    module_init(init_mod);    //マクロで関数を登録
+    module_exit(cleanup_mod); //同上
