c83bb360d593c1bf8000fb1e65e68e3ab2dcfcde
Bato Riko <s20c1101jd@s.chib>
2021-12-05 18:35:27 +0900

    test

--- myled/1myled.c
+++ myled/1myled.c
@@ -45,10 +45,10 @@ static ssize_t led_write(struct file *filp, const char *buf, size_t count, loff_
     int i;
     int mode = 0;
     int count_num = 0;
-    if (copy_from_user(&c, buf, sizeof(char)))
+    if (copy_from_user(&s, buf, sizeof(char)))
         return -EFAULT;
 
-    printk(KERN_INFO "receive %c\n", c);
+    printk(KERN_INFO "receive %s\n", c);
 
     if (c == 'E')
         mode = 1;
